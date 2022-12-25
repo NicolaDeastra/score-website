d24d0a8abb6332e619ab217b229b3a403ba8ab0e
nicastra <nicola@persissolo.id>
2022-12-25 21:25:54 +0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,101 +1,33 @@
 import ExternalLink from '@/components/external-link';
 import PageLayout from '@/components/page-layout';
-import {
-  Button,
-  Center,
-  Divider,
-  Heading,
-  HStack,
-  Image,
-  Link,
-  Skeleton,
-  Stack,
-  Text,
-  VStack,
-} from '@chakra-ui/react';
-import { Trans, useTranslation } from 'react-i18next';
-import { FiExternalLink } from 'react-icons/fi';
-import { ImSphere } from 'react-icons/im';
-import { GITHUB_PROFILE, WEBSITE } from 'src/constants';
+import axios from 'axios';
+import { Box, Heading, Stack } from '@chakra-ui/react';
+import useSWR from 'swr';
+
+const getAllEvent = async () => {
+  try {
+    const res = await axios.get(
+      'https://api.sofascore.com/api/v1/odds/1/featured-events/football'
+    );
+
+    return await res.data;
+  } catch (error) {
+    return error;
+  }
+};
 
 const IndexPage = () => {
-  const { t } = useTranslation();
+  const { data } = useSWR('/getLatestEvent', getAllEvent);
 
+  console.log(data);
   return (
-    <PageLayout
-      title='Home'
-      description='Discover a starter kit which includes Next.js, Chakra-UI, Framer-Motion in Typescript. You have few components, Internationalization, SEO and more in this template ! Enjoy coding.'
-    >
-      <Stack
-        spacing={4}
-        py={12}
-        align='center'
-        h='100vh'
-        w='100%'
-        direction={{ base: 'column', md: 'row' }}
-      >
-        <VStack
-          spacing={2}
-          align='start'
-          w={{ base: '100%', md: '50%' }}
-          py={{ base: 20, md: 0 }}
-        >
-          <Heading as='h1'>Alexandre GOSSARD</Heading>
-          <Divider />
-          <Text color='gray.500' align='justify'>
-            <Trans i18nKey='excerpt'>
-              <strong>Student</strong>
-              by day,
-              <strong>mad developer</strong>
-              by night. Passionate about computer science and
-              <strong>new technologies</strong>, currently
-              <ExternalLink href='https://nextjs.org/'>
-                Next.js
-              </ExternalLink>&{' '}
-              <ExternalLink href='https://chakra-ui.com/'>
-                Chakra UI
-              </ExternalLink>
-              , I develop in order to propose different{' '}
-              <strong>opensource</strong>
-              contents.
-            </Trans>
-          </Text>
-          <HStack
-            spacing={4}
-            w='full'
-            justify={{ base: 'center', md: 'flex-start' }}
-          >
-            <Link href={WEBSITE} isExternal>
-              <Button
-                colorScheme='brand'
-                variant='ghost'
-                rightIcon={<ImSphere />}
-              >
-                Website
-              </Button>
-            </Link>
-            <Link href={GITHUB_PROFILE} isExternal>
-              <Button
-                colorScheme='brand'
-                variant='ghost'
-                rightIcon={<FiExternalLink />}
-              >
-                Github
-              </Button>
-            </Link>
-          </HStack>
-        </VStack>
-        <Center w={{ base: '100%', md: '50%' }}>
-          <Image
-            src='/assets/images/home.jpg'
-            h={500}
-            fit='cover'
-            fallback={<Skeleton w={{ base: '100%', md: '50%' }} h={500} />}
-            alt=''
-          />
-        </Center>
+    <Box>
+      <Stack>
+        {data?.featuredEvents?.map((item, index) => (
+          <Heading key={index}>1</Heading>
+        ))}
       </Stack>
-    </PageLayout>
+    </Box>
   );
 };
 
---
+++ src/service/index.ts
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://divanscore.p.rapidapi.com';
+export const key = '38516c232amsh16080c0368b3c92p150d94jsnbb476d22368a';
+export const host = 'divanscore.p.rapidapi.com';
